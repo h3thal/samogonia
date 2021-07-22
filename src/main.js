a6deb022752f930b0ce7742892c97f76a7aa1596
h3thal <h3thal@yandex.ru>
2021-07-22 20:22:29 +0300

    vue-route 4

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+
+const Home = { template: '<div>Home</div>' }
+const About = { template: '<div>About</div>' }
+
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/about', component: About }
+  ] // short for `routes: routes`
+})
 
 const app = createApp(App)
 app.use(router)
